2e49be41dd6ac54af8eeb242032c1744e6332bd5
Thomas Maxwell <thomas@bluemagma.be>
2023-04-26 12:04:40 +0200

    showcase hyperscript bug

---
+++ src/main/java/be/bluemagma/jtehyperscriptbug/JteHyperscriptBugApplication.java
@@ -0,0 +1,13 @@
+package be.bluemagma.jtehyperscriptbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteHyperscriptBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JteHyperscriptBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/bluemagma/jtehyperscriptbug/JteHyperscriptBugApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bluemagma.jtehyperscriptbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JteHyperscriptBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
